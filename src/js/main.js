269f1d3ea4255bb5c6537f610ef5a1ac4056f80b
JuanJClR <juanj.calderon@upb.edu.co>
2024-08-28 23:16:58 -0500

    Modificados IDs y estructura del Home

--- src/js/main.js
+++ src/js/main.js
@@ -3,50 +3,52 @@ let btns = [];
 
 window.onload = init;
 
-function init(){
+function init() {
     refs["splash"] = document.getElementById("splash");
     refs["home"] = document.getElementById("home");
-    refs["juego"] = document.getElementById("juego");
-    refs["creditos"] = document.getElementById("creditos");
-    refs["instrucciones"] = document.getElementById("instrucciones");
+    refs["reservar"] = document.getElementById("reservar");
+    refs["karaoke"] = document.getElementById("karaoke");
+    refs["pedido"] = document.getElementById("pedido");
 
-    btns["btn_juego"] = document.getElementById("btn_juego");
-    btns["btn_creditos"] = document.getElementById("btn_creditos");
-    btns["btn_instrucciones"] = document.getElementById("btn_instrucciones");
+    btns["btn_reservar"] = document.getElementById("btn_reservar");
+    btns["btn_karaoke"] = document.getElementById("btn_karaoke");
+    btns["btn_pedido"] = document.getElementById("btn_pedido");
 
     asignarEventosMenu();
     asignarVolver();
 
-    setTimeout(()=>{
+    setTimeout(() => {
         cargarSeccion("home");
     }, 3000);
 }
-function asignarVolver(){
+
+function asignarVolver() {
     let btns_volver = document.querySelectorAll(".volver");
     for (let i = 0; i < btns_volver.length; i++) {
-        btns_volver[i].addEventListener("click", ()=>{
+        btns_volver[i].addEventListener("click", () => {
             cargarSeccion("home");
         });
     }
 }
-function asignarEventosMenu()
-{
-    btns["btn_juego"].addEventListener("click", cambiarSeccion);
-    btns["btn_creditos"].addEventListener("click", cambiarSeccion);
-    btns["btn_instrucciones"].addEventListener("click", cambiarSeccion);
+
+function asignarEventosMenu() {
+    btns["btn_reservar"].addEventListener("click", cambiarSeccion);
+    btns["btn_karaoke"].addEventListener("click", cambiarSeccion);
+    btns["btn_pedido"].addEventListener("click", cambiarSeccion);
 }
-function ocultar()
-{
+
+function ocultar() {
     for (let key in refs) {
         refs[key].classList.add("ocultar");
     }
 }
-function cambiarSeccion(e){ 
+
+function cambiarSeccion(e) {
     let seccion = e.target.id.split("_")[1];
     cargarSeccion(seccion);
 }
 
-function cargarSeccion(seccion){
+function cargarSeccion(seccion) {
     ocultar();
     refs[seccion].classList.remove("ocultar");
     refs[seccion].classList.add("animate__animated", "animate__fadeIn");
